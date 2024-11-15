fa5a4edb95925f6aafacd2056596299647fcc4db
maryna <276069@student.pwr.edu.pl>
2024-11-15 17:43:45 +0100

    Db connection implemented

---
+++ backend/we_backend/we_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class WeAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'we_app'
--- backend/we_backend/we_backend/settings.py
+++ backend/we_backend/we_backend/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'we_app'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'we_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    'ENGINE': 'django.db.backends.postgresql',
+    'NAME': 'we-app-db',
+    'USER': 'postgres.fyxjuzfbmorzshodobaq',
+    'PASSWORD': 'WalkExplorerAdmin!',
+    'HOST': 'aws-0-eu-central-1.pooler.supabase.com',
+    'PORT': '6543',
     }
 }
 
