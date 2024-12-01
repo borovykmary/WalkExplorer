0322a07524f0f0bc91b4812a1c48e93a596ab882
maryna <276069@student.pwr.edu.pl>
2024-12-01 14:39:05 +0100

    main page components added: top navigation menu, walk request input modal, mapbox

---
+++ frontend/we-app/src/main_page/components/TopNavigation.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./TopNavigation.css";
+
+const TopNavigation = ({ closeMenu }) => {
+  return (
+    <>
+      <div className="menu-overlay" onClick={closeMenu}></div>
+
+      <div className="slide-menu open">
+        <button className="close-btn" onClick={closeMenu}>
+          ×
+        </button>
+        <div className="menu-content">
+          <div className="menu-item">Profile Settings</div>
+          <div className="menu-item">Home Page</div>
+          <button className="logout-btn">Log Out</button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default TopNavigation;
